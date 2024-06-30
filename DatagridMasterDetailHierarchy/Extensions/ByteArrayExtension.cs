8b8f71002f553198d4ccb8f4f9d3d18f237d60e8
codewithelmor <cabalfinelmor17@gmail.com>
2024-06-30 18:11:59 +0800

    Add project files.

---
+++ DatagridMasterDetailHierarchy/Extensions/ByteArrayExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace DatagridMasterDetailHierarchy.Extensions
+{
+    public static class ByteArrayExtension
+    {
+        public static string ToBase64String(this byte[] byteArrays)
+        {
+            string base64String = Convert.ToBase64String(byteArrays);
+            //string base64String = BitConverter.ToString(byteArrays);
+            string dataUri = "data:image/jpeg;base64," + base64String;
+            return dataUri;
+        }
+    }
+}
